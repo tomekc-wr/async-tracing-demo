df6862a2d313013f342ece714910df633ed26736
Filip Zeglen <fzeglen@worldremit.com>
2020-03-30 14:31:08 +0200

    spring cloud stream kafka binder support

---
+++ src/main/java/com/example/asynctracing/SomeBody.java
@@ -0,0 +1,15 @@
+package com.example.asynctracing;
+
+
+public class SomeBody {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
